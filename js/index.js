271335674705e26c3bdfdef31834bce073316b60
alannguyen19 <alan.nguyen@brighte.com.au>
2022-02-01 21:44:36 +1100

    Sprint 3 - Fix bugs and add enew features

--- js/index.js
+++ js/index.js
@@ -7,139 +7,130 @@ taskManager.render();
 //Finding and Displaying the Date
 const dateElement = document.querySelector("#date-element");
 let today = new Date();
-const [day, month, year] = [today.getDate(), today.getMonth()+1, today.getFullYear()];
+const [day, month, year] = [today.getDate(), today.getMonth() + 1, today.getFullYear()];
 console.log(today);
 let dateString = `Current Date: ${day} / ${month} / ${year}`;
 dateElement.innerHTML = dateString;
 
+const modalTaskForm = document.getElementById('taskForm');
+
+//focus to name field when modal load
+modalTaskForm.addEventListener('shown.bs.modal', function () {
+  newTaskNameInput.focus();
+});
 
 // Select the New Task Form
 const newTaskForm = document.querySelector('#newTaskForm');
 
 // Add an 'onsubmit' event listener
 newTaskForm.addEventListener("submit", (event) => {
-//implement a JavaScript function named validFormFieldInput(data)
+  //implement a JavaScript function named validFormFieldInput(data)
 
-    let newTaskNameInput = document.querySelector('#newTaskNameInput');
-    console.log("name:  " + newTaskNameInput.value);
-    let newTaskDescription = document.querySelector('#newTaskDescription');
-    console.log("description:  " + newTaskDescription.value);
-    let newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
-    console.log("assigned to:  " + newTaskAssignedTo.value);
-    let newTaskDueDate = document.querySelector('#newTaskDueDate');
-    console.log("due date:  " + newTaskDueDate.value);
-    let newTaskStatus = document.querySelector('#newTaskStatus');
-    console.log("status:  " + newTaskStatus.value);
-    let validationFail = 0;
-    
-    // Prevent default action
-    event.preventDefault();
+  let newTaskNameInput = document.querySelector('#newTaskNameInput');
+  console.log("name:  " + newTaskNameInput.value);
+  let newTaskDescription = document.querySelector('#newTaskDescription');
+  console.log("description:  " + newTaskDescription.value);
+  let newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
+  console.log("assigned to:  " + newTaskAssignedTo.value);
+  let newTaskDueDate = document.querySelector('#newTaskDueDate');
+  console.log("due date:  " + newTaskDueDate.value);
+  let newTaskStatus = document.querySelector('#newTaskStatus');
+  console.log("status:  " + newTaskStatus.value);
+  let validationFail = 0;
 
-     // Call this to clear all the form fields after the submission
-    const clearFormFields = () => {
-        newTaskNameInput.value = " ";
-        newTaskDescription.value = " ";
-        newTaskAssignedTo.value = " ";
-        newTaskStatus.value = "In Progress";
-        /*newTaskDueDate.value = " ";*/
-        newTaskNameInput.classList.remove("is-valid");
-        newTaskDescription.classList.remove("is-valid");
-        newTaskAssignedTo.classList.remove("is-valid");
-        newTaskStatus.classList.remove("is-valid");
-        newTaskDueDate.classList.remove("is-valid");
-    };
+  // Prevent default action
+  event.preventDefault();
 
-    // new date format
-    let todaysDate = new Date(Date.now())
-       .toLocaleString()
-       .split(",")[0]
-       .split("/");
-    let day = todaysDate[0];
-    let month = todaysDate[1];
-    let year = todaysDate[2];
+  // Call this to clear all the form fields after the submission
+  const clearFormFields = () => {
+    newTaskNameInput.value = " ";
+    newTaskDescription.value = " ";
+    newTaskAssignedTo.value = " ";
+    newTaskStatus.value = "In Progress";
+    /*newTaskDueDate.value = " ";*/
+    newTaskNameInput.classList.remove("is-valid");
+    newTaskDescription.classList.remove("is-valid");
+    newTaskAssignedTo.classList.remove("is-valid");
+    newTaskStatus.classList.remove("is-valid");
+    newTaskDueDate.classList.remove("is-valid");
+  };
+
+  // new date format
+  let todaysDate = new Date(Date.now())
+    .toLocaleString()
+    .split(",")[0]
+    .split("/");
+  let day = todaysDate[0];
+  let month = todaysDate[1];
+  let year = todaysDate[2];
   /* taskDueDate is in yyyy-mm-dd format
     let taskDueDate = validatenewTaskDueDate.value.split("-");*/
 
-// Form validation for all the Task Field 
-    if (newTaskNameInput.value.length > 5) {
-        newTaskNameInput.classList.add("is-valid");
-        newTaskNameInput.classList.remove("is-invalid");
-        } else {
-            newTaskNameInput.classList.add("is-invalid");
-            newTaskNameInput.classList.remove("is-valid");
-            validationFail++;
-        }
+  // Form validation for all the Task Field
+  if (newTaskNameInput.value.length > 5) {
+    newTaskNameInput.classList.add("is-valid");
+    newTaskNameInput.classList.remove("is-invalid");
+  } else {
+    newTaskNameInput.classList.add("is-invalid");
+    newTaskNameInput.classList.remove("is-valid");
+    validationFail++;
+  }
 
-    if (newTaskDescription.value.length > 5) {
-        newTaskDescription.classList.add("is-valid");
-        newTaskDescription.classList.remove("is-invalid");
-        } else {
-            newTaskDescription.classList.add("is-invalid");
-            newTaskDescription.classList.remove("is-valid");
-            validationFail++;
-        }
-    
-    if (newTaskAssignedTo.value.length > 5) {
-        newTaskAssignedTo.classList.add("is-valid");
-        newTaskAssignedTo.classList.remove("is-invalid");
-        } else {
-            newTaskAssignedTo.classList.add("is-invalid");
-            newTaskAssignedTo.classList.remove("is-valid");
-            validationFail++;
-        }
- 
-    if (newTaskDueDate.value.length != 0) {
-        newTaskDueDate.classList.add("is-valid");
-        newTaskDueDate.classList.remove("is-invalid");
-        } else {
-            newTaskDueDate.classList.add("is-invalid");
-            newTaskDueDate.classList.remove("is-valid");
-            validationFail++;
-        }
-    if (newTaskStatus.value.length != 0) {
-        newTaskStatus.classList.add("is-valid");
-        newTaskStatus.classList.remove("is-invalid");
-        } else {
-            newTaskStatus.classList.add("is-invalid");
-            newTaskStatus.classList.remove("is-valid");
-            validationFail++;
-        }
-    
-    /*Form validation that is more advanced for finding due date, was a stretch goal in previous task
-    console.log(
-      `newTaskDueDate[2]:${taskDueDate[2]} day:${day} taskDueDate[1]:${taskDueDate[1]} month:${month} taskDueDate[0]:${taskDueDate[0]} year:${year}`
+  if (newTaskDescription.value.length > 5) {
+    newTaskDescription.classList.add("is-valid");
+    newTaskDescription.classList.remove("is-invalid");
+  } else {
+    newTaskDescription.classList.add("is-invalid");
+    newTaskDescription.classList.remove("is-valid");
+    validationFail++;
+  }
+
+  if (newTaskAssignedTo.value.length > 5) {
+    newTaskAssignedTo.classList.add("is-valid");
+    newTaskAssignedTo.classList.remove("is-invalid");
+  } else {
+    newTaskAssignedTo.classList.add("is-invalid");
+    newTaskAssignedTo.classList.remove("is-valid");
+    validationFail++;
+  }
+
+  if (newTaskDueDate.value.length != 0) {
+    newTaskDueDate.classList.add("is-valid");
+    newTaskDueDate.classList.remove("is-invalid");
+  } else {
+    newTaskDueDate.classList.add("is-invalid");
+    newTaskDueDate.classList.remove("is-valid");
+    validationFail++;
+  }
+  if (newTaskStatus.value.length != 0) {
+    newTaskStatus.classList.add("is-valid");
+    newTaskStatus.classList.remove("is-invalid");
+  } else {
+    newTaskStatus.classList.add("is-invalid");
+    newTaskStatus.classList.remove("is-valid");
+    validationFail++;
+  }
+
+  if (validationFail > 0) {
+    validationFail = 0;
+    return;
+  } else {
+    // Push the valid input into our tasks array
+    taskManager.addTask(
+      newTaskNameInput.value,
+      newTaskDescription.value,
+      newTaskAssignedTo.value,
+      newTaskDueDate.value,
+      newTaskStatus.value,
     );
-    if (
-      taskDueDate[2] >= day &&
-      taskDueDate[1] >= month &&
-      taskDueDate[0] >= year
-    ) {
-      newTaskDueDate.classList.add("is-valid");
-      newTaskDueDate.classList.remove("is-invalid");
-    } else {
-      newTaskDueDate.classList.add("is-invalid");
-      newTaskDueDate.classList.remove("is-valid");
-      validationFail++;
-    } */
-// If validation fails then function will not proceed further and
-// will return. The value of validationFail will also needed to be
-// reset to 0.
-   
-    if (validationFail > 0) {
-        validationFail = 0;
-        return;
-    } else {
-        // Push the valid input into our tasks array
-        taskManager.addTask(
-          newTaskNameInput.value,
-          newTaskDescription.value,
-          newTaskAssignedTo.value,
-          newTaskDueDate.value,
-          newTaskStatus.value,
-        );
-        clearFormFields();
-        taskManager.render();
-        }    
+    clearFormFields();
+    taskManager.save();
+
+    //hide modal form
+    $('#taskForm').modal('hide');
+
+    taskManager.render();
+  }
 });
 
 const taskList = document.querySelector("#task-list");
@@ -150,8 +141,8 @@ taskList.addEventListener("click", (event) => {
     // Get the correct parent Task, yours might be slightly different
     // Use console.log(event.target.parentElement) to see
     const parentTask =
-      event.target.parentElement.parentElement.parentElement.parentElement.parentElement;
-      console.log(parentTask);
+      event.target.parentElement.parentElement.parentElement.parentElement;
+    console.log(parentTask);
     // Get the taskId of the parent Task and turn it into a number.
     const taskId = Number(parentTask.dataset.taskId);
     // Get the task from the TaskManager using the taskId
@@ -160,7 +151,7 @@ taskList.addEventListener("click", (event) => {
     task.status = "Done";
     taskManager.save();
     // Render the tasks
-    taskManager.render(); 
+    taskManager.render();
     console.log(taskManager);
   }
   // Check if a "Delete" button was clicked
@@ -180,12 +171,5 @@ taskList.addEventListener("click", (event) => {
 
     // Render the tasks
     taskManager.render();
-  }  
-  
-
-
-  });
-
-
-
-
+  }
+});
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,14 +1,8 @@
 const createTaskHtml = (name, description, assignedTo, dueDate, status, id) => {
-    const html = `
+  const html = `
 <li class="list-group-item" data-task-id="${id}">
     <div class="widget-content p-0">
         <div class="widget-content-wrapper">
-            <!--<div class="widget-content-left mr-2">
-                <div class="custom-checkbox custom-control">
-                    <input class="input" type="checkbox">
-                    <label class="label">&nbsp;</label>
-                </div>
-            </div>-->
             <div class="widget-content-left">
                 <div class="widget-heading">Name: ${name}</div>
                 <div class="widget-description">Description: ${description}</div>
@@ -16,51 +10,46 @@ const createTaskHtml = (name, description, assignedTo, dueDate, status, id) => {
                 <div class="widget-subheading"><i>Due Date: ${dueDate}</i></div>
                 <div class="widget-subheading"><i>Status: ${status}</i></div>
             </div>
-                <!--<button class="btn btn-outline-success done-button">
-                Done
-                </button>-->
             <div class="widget-content-right">
                 <button class="btn btn-outline-danger delete-button">
                 Delete
                 </button>
-                
-                <button class="border-0 btn-transition btn btn-outline-success"
-                    data-bs-toggle="modal" data-bs-target="#taskForm">
-                    <i class="fa fa-check-circle done-button "></i>
+                <button class="btn btn-outline-success done-button ${(status === 'In progress') ? 'visible' : 'invisible'}">
+                    Done
                 </button>
             </div>
         </div>
     </div>
 </li>
 `;
-return html;
+  return html;
 
 };
 // Create the TaskManager class
 class TaskManager {
-    constructor(currentId = 0) {
-      this.tasks = [];
-      this.currentId = currentId;     
-    }
-// Create the addTask method
-addTask(name, description, assignedTo, dueDate, status) {
-// Create a task object that we will push to the list of tasks
-    
-const task = {
-// Increment the current Id for each new task
-    id: this.currentId++,
-    name: name,
-    description: description,
-    assignedTo: assignedTo,
-    dueDate: dueDate,
-    status: status,
-  };
-  console.log(task);
-  this.tasks.push(task);  
-  console.log(this.tasks);
+  constructor(currentId = 0) {
+    this.tasks = [];
+    this.currentId = currentId;
+  }
+  // Create the addTask method
+  addTask(name, description, assignedTo, dueDate, status) {
+    // Create a task object that we will push to the list of tasks
 
-}
-getTaskById(taskId) {
+    const task = {
+      // Increment the current Id for each new task
+      id: this.currentId++,
+      name: name,
+      description: description,
+      assignedTo: assignedTo,
+      dueDate: dueDate,
+      status: status,
+    };
+    console.log(task);
+    this.tasks.push(task);
+    console.log(this.tasks);
+
+  }
+  getTaskById(taskId) {
     // Create a variable to store the found task
     let foundTask;
     // Loop over the tasks and find the task with the id passed as a parameter
@@ -75,41 +64,42 @@ getTaskById(taskId) {
     }
     // Return the found task
     return foundTask;
-}
-//Create the render method
-render () {
+  }
+  //Create the render method
+  render() {
     let tasksHtmlList = [];
     // Loop over our tasks and create the html, storing it in the array
     for (let i = 0; i < this.tasks.length; i++) {
-        // Get the current task in the loop
-        const task = this.tasks[i];
-        // Format the date
-        const date = new Date(task.dueDate);
-        const formattedDate =
-          date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
-        // Create the task html
-        const taskHtml = createTaskHtml(
-          task.name,
-          task.description,
-          task.assignedTo,
-          formattedDate,
-          task.status,
-          task.id,
-        );
-        // Push it to the tasksHtmlList array
-        tasksHtmlList.push(taskHtml);
+      // Get the current task in the loop
+      const task = this.tasks[i];
+      // Format the date
+      const date = new Date(task.dueDate);
+      const formattedDate =
+        date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+      // Create the task html
+      const taskHtml = createTaskHtml(
+        task.name,
+        task.description,
+        task.assignedTo,
+        formattedDate,
+        task.status,
+        task.id,
+      );
+      // Push it to the tasksHtmlList array
+      tasksHtmlList.push(taskHtml);
 
-}
-// Create the tasksHtml by joining each item in the tasksHtmlList
-// with a new line in between each item.
+    }
+    // Create the tasksHtml by joining each item in the tasksHtmlList
+    // with a new line in between each item.
     const tasksHtml = tasksHtmlList.join("\n");
 
-// Set the inner html of the tasksList on the page
+    // Set the inner html of the tasksList on the page
     const tasksList = document.querySelector("#task-list");
     tasksList.innerHTML = tasksHtml;
-}
+  }
 
-save() {
+  save() {
+    console.log(this.tasks);
     // Create a JSON string of the tasks
     const tasksJson = JSON.stringify(this.tasks);
 
@@ -141,26 +131,25 @@ save() {
       // Convert the currentId to a number and store it in our TaskManager
       this.currentId = Number(currentId);
     }
-    }
-    deleteTask(taskId) {
-        // Create an empty array and store it in a new variable, newTasks
-        const newTasks = [];
-    
-        // Loop over the tasks
-        for (let i = 0; i < this.tasks.length; i++) {
-          // Get the current task in the loop
-          const task = this.tasks[i];
-    
-          // Check if the task id is not the task id passed in as a parameter
-          if (task.id !== taskId) {
-            // Push the task to the newTasks array
-            newTasks.push(task);
-          }
-        }
-    
-        // Set this.tasks to newTasks
-        this.tasks = newTasks;
-        
-        
+  }
+
+  deleteTask(taskId) {
+    // Create an empty array and store it in a new variable, newTasks
+    const newTasks = [];
+
+    // Loop over the tasks
+    for (let i = 0; i < this.tasks.length; i++) {
+      // Get the current task in the loop
+      const task = this.tasks[i];
+
+      // Check if the task id is not the task id passed in as a parameter
+      if (task.id !== taskId) {
+        // Push the task to the newTasks array
+        newTasks.push(task);
       }
+    }
+
+    // Set this.tasks to newTasks
+    this.tasks = newTasks;
+  }
 }
